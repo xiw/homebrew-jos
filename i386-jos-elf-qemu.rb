46718ecc376fe0f0bfa10bce389ab3625eb02864
Xi Wang <xi.wang@gmail.com>
2015-08-23 17:13:52 -0700

    add qemu

---
+++ i386-jos-elf-qemu.rb
@@ -0,0 +1,41 @@
+class I386JosElfQemu < Formula
+
+  desc "x86 Emulator"
+  homepage "http://wiki.qemu.org"
+  head "https://github.com/geofft/qemu.git", :branch => "6.828-2.3.0"
+  conflicts_with "qemu"
+
+  depends_on "pkg-config" => :build
+  depends_on "libtool" => :build
+  depends_on "jpeg"
+  depends_on "gnutls"
+  depends_on "glib"
+  depends_on "pixman"
+  depends_on "vde" => :optional
+  depends_on "sdl" => :optional
+  depends_on "gtk+" => :optional
+  depends_on "libssh2" => :optional
+
+  def install
+    ENV["LIBTOOL"] = "glibtool"
+
+    args = %W[
+      --target-list=i386-softmmu
+      --prefix=#{prefix}
+      --cc=#{ENV.cc}
+      --host-cc=#{ENV.cc}
+      --enable-cocoa
+      --disable-bsd-user
+      --disable-guest-agent
+      --disable-kvm
+    ]
+
+    args << (build.with?("sdl") ? "--enable-sdl" : "--disable-sdl")
+    args << (build.with?("vde") ? "--enable-vde" : "--disable-vde")
+    args << (build.with?("gtk+") ? "--enable-gtk" : "--disable-gtk")
+    args << (build.with?("libssh2") ? "--enable-libssh2" : "--disable-libssh2")
+
+    system "./configure", *args
+    system "make", "V=1", "install"
+  end
+end
