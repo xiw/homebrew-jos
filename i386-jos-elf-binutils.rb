ff863507620a9579a4715bc6065df65130e1a38b
Xi Wang <xi.wang@gmail.com>
2016-09-19 11:02:52 -0700

    sync binutils to 2.27

--- i386-jos-elf-binutils.rb
+++ i386-jos-elf-binutils.rb
@@ -2,9 +2,9 @@ class I386JosElfBinutils < Formula
 
   desc "FSF Binutils for i386-elf development"
   homepage "https://www.gnu.org/software/binutils/binutils.html"
-  url "http://ftpmirror.gnu.org/binutils/binutils-2.25.1.tar.gz"
-  mirror "https://ftp.gnu.org/gnu/binutils/binutils-2.25.1.tar.gz"
-  sha256 "82a40a37b13a12facb36ac7e87846475a1d80f2e63467b1b8d63ec8b6a2b63fc"
+  url "https://ftpmirror.gnu.org/binutils/binutils-2.27.tar.gz"
+  mirror "https://ftp.gnu.org/gnu/binutils/binutils-2.27.tar.gz"
+  sha256 "26253bf0f360ceeba1d9ab6965c57c6a48a01a8343382130d1ed47c468a3094f"
 
   def install
     args = [
