a9bcc56fe70a205d370e1883d4c45aa6d8f3b836
Xi Wang <xi.wang@gmail.com>
2016-09-20 16:36:00 -0700

    sync with gcc 6.2.0 & add go

--- i386-jos-elf-gcc.rb
+++ i386-jos-elf-gcc.rb
@@ -2,9 +2,9 @@ class I386JosElfGcc < Formula
 
   desc "GNU compiler collection for i386-elf development"
   homepage "https://gcc.gnu.org"
-  url "http://ftpmirror.gnu.org/gcc/gcc-5.3.0/gcc-5.3.0.tar.bz2"
-  mirror "https://ftp.gnu.org/gnu/gcc/gcc-5.3.0/gcc-5.3.0.tar.bz2"
-  sha256 "b84f5592e9218b73dbae612b5253035a7b34a9a1f7688d2e1bfaaf7267d5c4db"
+  url "https://ftpmirror.gnu.org/gcc/gcc-6.2.0/gcc-6.2.0.tar.bz2"
+  mirror "https://ftp.gnu.org/gnu/gcc/gcc-6.2.0/gcc-6.2.0.tar.bz2"
+  sha256 "9944589fc722d3e66308c0ce5257788ebd7872982a718aa2516123940671b7c5"
 
   depends_on "gmp"
   depends_on "libmpc"
@@ -12,9 +12,9 @@ class I386JosElfGcc < Formula
   depends_on "i386-jos-elf-binutils"
 
   def install
-    languages = %w[c]
+    languages = %w[c go]
 
-    binutils = Formula.factory "i386-jos-elf-binutils"
+    binutils = Formulary.factory "i386-jos-elf-binutils"
 
     args = [
       "--prefix=#{prefix}",
